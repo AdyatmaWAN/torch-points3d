3bf34894428b3dd75715da48789bd1ac534fa1d0
Adyatma <adyatma.wijaksara@gmail.com>
2024-10-21 17:25:15 +0700

    get current directory to debug file paths

--- torch_points3d/datasets/change_detection/Urb3DCD_cls.py
+++ torch_points3d/datasets/change_detection/Urb3DCD_cls.py
@@ -253,6 +253,7 @@ class Urb3DCD_cls_cylinder(Dataset):
         self.filesPC0 = []
         self.filesPC1 = []
         self.labels = []
+        print(os.getcwd())
         globPath = os.scandir(self.filePaths)
         for dir in globPath:
             if dir.is_dir():
